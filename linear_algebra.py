84d528850e2b0867e4c3d307663f556e85199427
Oren Koriat <oren@cynerio.co>
2021-05-29 20:35:52 +0300

    rotate full image

--- linear_algebra.py
+++ linear_algebra.py
@@ -48,18 +48,32 @@ def rotate_left(point_vector: Tuple[int, int]):
     return left_rotation_matrix @ point_vector
 
 
-if __name__ == "__main__":
-    point = (10, 10)
-    img = np.zeros((100, 100))
-    points = [point]
-    for p in points:
-        set_point(p, img)
-    Image.fromarray(img).show()
+def rotate_image_right(image: ndarray):
+    rotated_image = np.zeros(image.shape)
+    for p_vec in np.transpose(np.nonzero(image)):
+        set_point(rotate_right(p_vec), rotated_image)
+    return rotated_image
 
-    # rotate point right ones
-    img = np.zeros((100, 100))
-    points = [point]
-    for p in points:
-        x, y = rotate_left(p)
-        img[x][y] = 255.
+
+def rotate_image_left(image: ndarray):
+    rotated_image = np.zeros(image.shape)
+    for point_vector in np.transpose(np.nonzero(image)):
+        set_point(rotate_left(point_vector), rotated_image)
+    return rotated_image
+
+
+if __name__ == "__main__":
+    img = np.identity(100)
+    for point in np.transpose(np.nonzero(img)):
+        set_point(point, img)
+    for point in [(10, 10), (10, 20), (10, 30), (10, 40)]:
+        set_point(point, img)
+
+    rot1 = rotate_image_right(img)
+    rot2 = rotate_image_right(rot1)
+    rot3 = rotate_image_right(rot2)
+    rot4 = rotate_image_right(rot3)
     Image.fromarray(img).show()
+    Image.fromarray(rot1).show()
+    Image.fromarray(rot2).show()
+    Image.fromarray(rot3).show()
