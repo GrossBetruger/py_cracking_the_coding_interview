25ef81d5d5b4f3f19df0fdecab32faaa6351bbb0
Oren Koriat <oren@cynerio.co>
2021-05-29 23:40:42 +0300

    add shear transformations, show images side by side

--- linear_algebra.py
+++ linear_algebra.py
@@ -28,6 +28,23 @@ def set_point(point: Tuple[int, int], img: ndarray):
     img[x][y] = 255.
 
 
+def apply_shear(point_vector: Tuple[int, int]):
+    shear = array(
+        [
+            [1, 1],
+            [0, 1]
+        ]
+    )
+    return shear @ point_vector
+
+
+def apply_shear_to_matrix(image: ndarray):
+    sheared_image = np.zeros(image.shape)
+    for p_vec in np.transpose(np.nonzero(image)):
+        set_point(apply_shear(p_vec), sheared_image)
+    return sheared_image
+
+
 def rotate_right(point_vector: Tuple[int, int]):
     right_rotation_matrix = array(
         [
@@ -63,7 +80,8 @@ def rotate_image_left(image: ndarray):
 
 
 if __name__ == "__main__":
-    img = np.identity(100)
+    # Rotation transformations
+    img = np.identity(200)
     for point in np.transpose(np.nonzero(img)):
         set_point(point, img)
     for point in [(10, 10), (10, 20), (10, 30), (10, 40)]:
@@ -73,7 +91,20 @@ if __name__ == "__main__":
     rot2 = rotate_image_right(rot1)
     rot3 = rotate_image_right(rot2)
     rot4 = rotate_image_right(rot3)
-    Image.fromarray(img).show()
-    Image.fromarray(rot1).show()
-    Image.fromarray(rot2).show()
-    Image.fromarray(rot3).show()
+    upper_side = np.hstack((img, rot1))
+    lower_side = np.hstack((rot3, rot2))
+    Image.fromarray(np.vstack((upper_side, lower_side))).show()
+
+    # Shear transformations
+    img = np.zeros((200, 200))
+    for point in [(10, 10), (10, 20), (10, 30)]:
+        set_point(point, img)
+    shear1 = apply_shear_to_matrix(img)
+    shear2 = apply_shear_to_matrix(shear1)
+    shear3 = apply_shear_to_matrix(shear2)
+    shear4 = apply_shear_to_matrix(shear3)
+    shear5 = apply_shear_to_matrix(shear4)
+    shear6 = apply_shear_to_matrix(shear5)
+    upper_side = np.hstack((shear1, shear2, shear3))
+    lower_side = np.hstack((shear4, shear5, shear6))
+    Image.fromarray(np.vstack((upper_side, lower_side))).show()
