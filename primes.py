9df1e06afa5a59e496b0dfbf27ae1ba712016992
Oren Koriat <oren@cynerio.co>
2021-06-15 15:53:23 +0300

    primes: pattern of six tuples

---
+++ primes.py
@@ -0,0 +1,27 @@
+from numpy import sqrt
+from termcolor import colored
+
+text = colored('Hello, World!', 'red', attrs=['reverse', 'blink'])
+
+
+def is_prime(n: int) -> bool:
+    if n in [0, 1]:
+        return False
+
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+def number_six_pack_pattern():
+    for i in range(1, 1000):
+        printable_number = colored(str(i), 'red') if is_prime(i) else colored(str(i), 'blue')
+        print(printable_number, end=" ")
+        if i % 6 == 0:
+            print()
+
+
+if __name__ == "__main__":
+    number_six_pack_pattern()
